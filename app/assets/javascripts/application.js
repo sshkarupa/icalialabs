b6197039085f2b9157af4952529f208f0a853479
Alicia <alicia@icalialabs.com>
2014-07-07 11:20:28 -0500

    Adds slider code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,40 @@
-
+jQuery(document).ready(function($) {
+  var moveLeft, moveRight, slideCount, slideHeight, slideWidth, sliderUlWidth;
+  moveLeft = function() {
+    $("#slider ul.slides").animate({
+      left: +slideWidth
+    }, 200, function() {
+      $("#slider ul.slides li:last-child").prependTo("#slider ul.slides");
+      $("#slider ul.slides").css("left", "");
+    });
+  };
+  moveRight = function() {
+    $("#slider ul.slides").animate({
+      left: -slideWidth
+    }, 2000, function() {
+      $("#slider ul.slides li:first-child").appendTo("#slider ul.slides");
+      $("#slider ul.slides").css("left", "");
+    });
+  };
+  slideCount = $("#slider ul.slides li").length;
+  slideWidth = $("#slider ul.slides li").width();
+  slideHeight = $("#slider ul.slides li").height();
+  sliderUlWidth = slideCount * slideWidth;
+  $("#slider").css({
+    width: slideWidth,
+    height: slideHeight
+  });
+  $("#slider ul.slides").css({
+    width: sliderUlWidth,
+    marginLeft: -slideWidth
+  });
+  $("#slider ul li:last-child").prependTo("#slider ul");
+  $("a.control_prev").on('click', function(e) {
+    moveLeft();
+    e.preventDefault();
+  });
+  $("a.control_next").on('click', function(e) {
+    moveRight();
+    e.preventDefault();
+  });
+});
--- coffeescript/application.coffee
+++ coffeescript/application.coffee
@@ -1,1 +1,45 @@
+jQuery(document).ready ($) ->
+  moveLeft = ->
+    $("#slider ul.slides").animate
+      left: +slideWidth
+    , 200, ->
+      $("#slider ul.slides li:last-child").prependTo "#slider ul.slides"
+      $("#slider ul.slides").css "left", ""
+      return
+    return
+
+  moveRight = ->
+    $("#slider ul.slides").animate
+      left: -slideWidth
+    , 2000, ->
+      $("#slider ul.slides li:first-child").appendTo "#slider ul.slides"
+      $("#slider ul.slides").css "left", ""
+      return
+
+    return
+
+  slideCount = $("#slider ul.slides li").length
+  slideWidth = $("#slider ul.slides li").width()
+  slideHeight = $("#slider ul.slides li").height()
+  sliderUlWidth = slideCount * slideWidth
+  $("#slider").css
+    width: slideWidth
+    height: slideHeight
+
+  $("#slider ul.slides").css
+    width: sliderUlWidth
+    marginLeft: -slideWidth
+
+  $("#slider ul li:last-child").prependTo "#slider ul"
+  $("a.control_prev").on 'click', (e) ->
+    moveLeft()
+    e.preventDefault()
+    return
+
+  $("a.control_next").on 'click', (e) ->
+    moveRight()
+    e.preventDefault()
+    return
+
+  return
 
