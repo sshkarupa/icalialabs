7fb839be72c25e0d268f355a2833c8fd49952e74
Scarlett Garza <scarlettgarza@Scarletts-MacBook-Pro.local>
2016-06-03 14:35:38 -0500

    adds view all section

--- source/javascripts/team_slider.js
+++ source/javascripts/team_slider.js
@@ -10,3 +10,18 @@ $(".members-nav a").click(function(){
 	$('.active').removeClass('active');
   $(this).addClass('active');
 });
+
+$(".hide").click(function(){
+	$('.members-fullpage').fadeOut();
+	$('.members-grid').fadeIn();
+});
+
+$('.members-grid .col-2').click(function() {
+	var member = $(this).attr('data-name');
+	$('.active').removeClass('active');
+	$('a[data-name="'+ member +'"]').addClass('active');
+	$('.members-fullpage').fadeIn();
+	$('.members-grid').fadeOut();
+	$('.member').hide();
+	$('.member[data-name="'+ member +'"]').show();
+});
