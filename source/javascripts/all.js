58264973bbefcdfe119726a5d89b600a3c2b067f
Scarlett Garza <scarlettgarza@Scarletts-MacBook-Pro.local>
2016-10-27 17:03:07 -0500

    delete unnecesary javascripts

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,13 +1,10 @@
 //= require application
 //= require classie
-//= require animsition
-//= require furatto
-//= require suraido
 //= require jquery.kinetic.min
 //= require jquery-waypoints
 //= require jquery.easing.min
 //= require process-scroller
-//= require furatto_modal
 //= require player
 //= require team_slider
 //= require jquery.scroller
+//= require typeit.min
