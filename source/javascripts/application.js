0ede019e6dc1ea98233e07e1ad20748cc1713708
Scarlett Garza <scarlettgarza@Scarletts-MacBook-Pro.local>
2016-10-28 12:04:19 -0500

    clenans up applications layouts and remove unnecesary js

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -71,3 +71,14 @@ $(window).scroll(function(){
     $('.main-menu').removeClass('is-active');
   }
 });
+
+$('.selected-works-slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  arrows: true,
+  prevArrow: '<a class="prev-arrow">Prev</a>',
+  nextArrow: '<a class="next-arrow">Next</a>'
+});
